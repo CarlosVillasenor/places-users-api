c18313a5e2a3526272d74a93b949d94b148007f6
CarlosVillasenor <carlosdeveloper9@gmail.com>
2026-09-15 12:04:26 -0600

    chore: remove unused uuid import
    Remove the unused uuid dependency import from the places controller.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,4 @@
 const HttpError = require("../models/http-error");
-const uuid = require("uuid");
 const { validationResult } = require("express-validator");
 const { getCoordsForAddress } = require("../util/location");
 const Place = require("../models/place");
