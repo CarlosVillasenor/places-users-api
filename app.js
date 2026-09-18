3b793fb8bbbc5a10d773b3734141eb79980bc535
CarlosVillasenor <carlosdeveloper9@gmail.com>
2026-09-18 12:43:59 -0600

    chore: format app entrypoint
    Normalize quote style and formatting in the Express application entrypoint.

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
  * starts the HTTP server.
  */
 require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 /**
  * Express application configured with the API routes for places.
@@ -25,29 +25,32 @@ app.use(bodyParser.json());
  * requests from any origin and specify allowed headers and methods.
  */
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
 
 /**
  * Registers places endpoints under the /api/places base path.
  */
-app.use('/api/places', placesRoutes);
+app.use("/api/places", placesRoutes);
 
 /**
  * Registers user endpoints under the /api/users base path.
  */
-app.use('/api/users', usersRoutes);
+app.use("/api/users", usersRoutes);
 
 /**
  * Handles requests that do not match a registered API endpoint.
  */
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
-}); 
+});
 
 /**
  * Converts errors from route handlers into JSON HTTP responses.
@@ -61,11 +64,11 @@ app.use((error, req, res, next) => {
   });
 });
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     app.listen(5000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
-
